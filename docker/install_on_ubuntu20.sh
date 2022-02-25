5be93ca0f86d5fe394006470eff1600d764a76e0
ety001 <work@akawa.ink>
2022-02-25 14:15:08 +0800

    update

---
+++ docker/install_on_ubuntu20.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt update -y && \
+apt install -y apt-transport-https ca-certificates curl software-properties-common && \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && \
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && \
+apt update -y && \
+apt install -y docker-ce
